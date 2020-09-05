ba6bc83bcc6fd2e31f07c311343cd32da4b74268
lalitvashishta <lalit.kvashishta@gmail.com>
2020-09-05 05:35:32 +0530

    lightbox added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const slides = document.querySelector(".slider-wrapper").children;
 const indicator = document.querySelector(".indicator");
-let index = 0;
+let slideIndex = 0;
 
 function nextSlide() {
-  if (index == slides.length - 1) {
-    index = 0;
+  if (slideIndex == slides.length - 1) {
+    slideIndex = 0;
   } else {
-    index++;
+    slideIndex++;
   }
   changeSlide();
 }
@@ -14,7 +14,6 @@ function nextSlide() {
 function circleIndicator() {
   for (let i = 0; i < slides.length; i++) {
     const div = document.createElement("div");
-    div.innerHtml = i + 1;
     div.setAttribute("onclick", "indicateSlide(this)");
     div.id = i;
     if (i == 0) {
@@ -26,7 +25,7 @@ function circleIndicator() {
 circleIndicator();
 
 function indicateSlide(element) {
-  index = element.id;
+  slideIndex = element.id;
   changeSlide();
   updateCircleIndicator();
   resetTimer();
@@ -36,14 +35,14 @@ function updateCircleIndicator() {
   for (let i = 0; i < indicator.children.length; i++) {
     indicator.children[i].classList.remove("active");
   }
-  indicator.children[index].classList.add("active");
+  indicator.children[slideIndex].classList.add("active");
 }
 
 function changeSlide() {
   for (let i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
   }
-  slides[index].classList.add("active");
+  slides[slideIndex].classList.add("active");
 }
 
 function resetTimer() {
@@ -57,3 +56,46 @@ function autoPlay() {
 }
 
 let timer = setInterval(autoPlay, 5000);
+
+/*------------Pop up-------------------- */
+const photoGallery = document.querySelector(".photos-wrapper").children;
+const lightBoxContainer = document.querySelector(".lightbox");
+const lightBoxImage = document.querySelector(".lightbox-img");
+let imgSrc;
+let index;
+
+for (let j = 0; j < photoGallery.length; j++) {
+  photoGallery[j].addEventListener("click", function () {
+    index = j;
+    lightBox();
+    changeImage();
+  });
+}
+
+function next() {
+  if (index == photoGallery.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeImage();
+}
+
+function prev() {
+  if (index == 0) {
+    index == photoGallery.length - 1;
+  } else {
+    index--;
+  }
+  changeImage();
+}
+
+function lightBox() {
+  lightBoxContainer.classList.toggle("open");
+}
+function changeImage() {
+  imgSrc = photoGallery[index].querySelector("img").getAttribute("src");
+  lightBoxImage.src = imgSrc;
+}
+
+/*------------Pop up-------------------- */
